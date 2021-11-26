8cf1e4e3e3afe4d0523f385b03b181cbac31b588
SakshamGarg1 <86765712+SakshamGarg1@users.noreply.github.com>
2021-11-26 18:52:09 +0530

    Update index.js

--- block-BJaacf/code/index.js
+++ block-BJaacf/code/index.js
@@ -2,19 +2,67 @@
 let numbers = ["1","2","3","4","5",];
 
 // 2. Calculate the sum of array items and print it to the console using console.log()
-let sum = numbers.arr[]
+let sum = 0
+for(let number of numbers){
+  sum+= number;
+}
+console.log(sum);
 // 3. Calculate the average of array items and print it to the console using console.log()
 
+
+console.log(sum/numbers.length);
 // 4. Find the highest number in the array and print it to the console using console.log()
+let highestNumber= number[0];
+
+for(let number of numbers){
+if(number>highestNumber){
+  highestNumber=number;
+}
+}
+console.log(highestNumber);
 
 // 5. Find the lowest number in the array and print it to the console using console.log()
+let lowestNumber= number[0];
+
+for(let number of numbers){
+if(number<lowestNumber){
+  lowestNumber=number;
+}
+}
+console.log(lowestNumber);
+
+
 
 // 6. Find the even numbers in the array and print them to the console using console.log()
 
+for(number of numbers){
+if(number % 2 ==0 ){
+  
+}
+  console.log(number);
+}
 // 7. Find the odd numbers in the array and print them to the console using console.log()
 
+for(number of numbers){
+if(number % 2 !==0 ){
+  
+}
+  console.log(number);
+
 // 8. Find the numbers in the array that is divisible by 5 and print them to the console using console.log()
+  for(number of numbers){
+if(number % 5 ==0 ){
+  
+}
+  console.log(number);
 
 // 9. Log all the element of the array one by one
+  for(number of numbers){
+  console.log(number,"each number");
 
 // 10. Find all the number in the array that is divisible by 3
+     for(number of numbers){
+if(number % 3==0 ){
+  
+}
+  console.log(number);
